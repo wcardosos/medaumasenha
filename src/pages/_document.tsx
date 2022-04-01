985b00ea5801777c2c4659ddd334fc62572a61f9
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-01 15:53:14 -0300

    fix(document) Fix font display

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Lato:wght@400;700&display=optional" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Lato:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
