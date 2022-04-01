dd429bda956adb877817be0807ba1b90bd99e40f
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-01 10:51:50 -0300

    feat(components/Form) Add numeric password validation

--- tests/components/Form.spec.tsx
+++ tests/components/Form.spec.tsx
@@ -16,46 +16,86 @@ describe('Component: Form', () => {
     cleanup();
   });
 
-  it('Should render the error message when a password type is not selected', async() => {
-    const user = userEvent.setup();
+  describe('General tests', () => {
+    it('Should render the error message when a password type is not selected', async() => {
+      const user = userEvent.setup();
 
-    await user.click(screen.getByTestId('generate-button'));
+      await user.click(screen.getByTestId('generate-button'));
 
-    expect(screen.getByText('É necessário escolher um tipo de senha!'));
-    expect(onSubmitSpy).not.toHaveBeenCalled();
+      expect(screen.getByText('É necessário escolher um tipo de senha!'));
+      expect(onSubmitSpy).not.toHaveBeenCalled();
+    });
   });
 
-  it('Should render the error message when characters quantity is less than 6', async() => {
-    const user = userEvent.setup();
+  describe('Alphanumeric flow specific tests', () => {
+    it('Should render the error message when characters quantity is less than 6', async() => {
+      const user = userEvent.setup();
 
-    await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
-    await user.type(screen.getByLabelText('Quantidade de caracteres:'), '5');
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
+      await user.type(screen.getByLabelText('Quantidade de caracteres:'), '5');
 
-    await user.click(screen.getByTestId('generate-button'));
+      await user.click(screen.getByTestId('generate-button'));
 
-    expect(screen.getByText('Valor mínimo: 6 caracteres'));
-    expect(onSubmitSpy).not.toHaveBeenCalled();
+      expect(screen.getByText('Valor mínimo: 6 caracteres'));
+      expect(onSubmitSpy).not.toHaveBeenCalled();
+    });
+
+    it('Should render the error message when characters quantity is more than 36', async() => {
+      const user = userEvent.setup();
+
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
+      await user.type(screen.getByLabelText('Quantidade de caracteres:'), '37');
+
+      await user.click(screen.getByTestId('generate-button'));
+
+      expect(screen.getByText('Valor máximo: 36 caracteres'));
+      expect(onSubmitSpy).not.toHaveBeenCalled();
+    });
+
+    it('Should generate a alphanumeric password', async() => {
+      const user = userEvent.setup();
+
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
+
+      await user.click(screen.getByTestId('generate-button'));
+
+      expect(onSubmitSpy).toHaveBeenCalledWith('alpha', null);
+    });
   });
 
-  it('Should render the error message when characters quantity is more than 36', async() => {
-    const user = userEvent.setup();
+  describe('Numeric flow specific tests', () => {
+    it('Should render the error message when characters quantity is less than 6', async() => {
+      const user = userEvent.setup();
 
-    await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
-    await user.type(screen.getByLabelText('Quantidade de caracteres:'), '37');
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'numeric');
+      await user.type(screen.getByLabelText('Quantidade de caracteres:'), '5');
 
-    await user.click(screen.getByTestId('generate-button'));
+      await user.click(screen.getByTestId('generate-button'));
 
-    expect(screen.getByText('Valor máximo: 36 caracteres'));
-    expect(onSubmitSpy).not.toHaveBeenCalled();
-  });
+      expect(screen.getByText('Valor mínimo: 6 caracteres'));
+      expect(onSubmitSpy).not.toHaveBeenCalled();
+    });
 
-  it('Should generate a alphanumeric password', async() => {
-    const user = userEvent.setup();
+    it('Should render the error message when characters quantity is more than 16', async() => {
+      const user = userEvent.setup();
 
-    await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'alpha');
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'numeric');
+      await user.type(screen.getByLabelText('Quantidade de caracteres:'), '17');
 
-    await user.click(screen.getByTestId('generate-button'));
+      await user.click(screen.getByTestId('generate-button'));
 
-    expect(onSubmitSpy).toHaveBeenCalledWith('alpha', null);
+      expect(screen.getByText('Valor máximo: 16 caracteres'));
+      expect(onSubmitSpy).not.toHaveBeenCalled();
+    });
+
+    it('Should generate a alphanumeric password', async() => {
+      const user = userEvent.setup();
+
+      await user.selectOptions(screen.getByLabelText('Tipo de senha:'), 'numeric');
+
+      await user.click(screen.getByTestId('generate-button'));
+
+      expect(onSubmitSpy).toHaveBeenCalledWith('numeric', null);
+    });
   });
 });
